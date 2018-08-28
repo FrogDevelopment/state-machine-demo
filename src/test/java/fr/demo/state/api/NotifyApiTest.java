495b844e651504d0d6a7d259dd5fe1003c7c6124
Benoit Le Gall <benoit.le-gall@monbanquet.fr>
2018-08-28 16:28:42 +0200

    ut correction

--- src/test/java/fr/demo/state/api/NotifyApiTest.java
+++ src/test/java/fr/demo/state/api/NotifyApiTest.java
@@ -1,24 +1,14 @@
 package fr.demo.state.api;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import fr.demo.state.common.AbstractStatePersist;
-import fr.demo.state.common.DemoEvent;
-import fr.demo.state.common.DemoState;
 import fr.demo.state.common.What;
 import fr.demo.state.order.OrderEvent;
-import fr.demo.state.order.OrderState;
 import fr.demo.state.order.data.OrderStatePersist;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.MediaType;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -28,7 +18,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -42,92 +32,36 @@ public class NotifyApiTest {
     @Mock
     private OrderStatePersist orderStatePersist;
 
-    // ************************ \\
-    //         MOCK MVC         \\
-    // ************************ \\
-
-    // cf https://memorynotfound.com/unit-test-spring-mvc-rest-service-junit-mockito/
-    private MockMvc mockMvc;
-
-    private static final MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    static {
-        objectMapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
-        objectMapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
-
-        mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
-    }
-
-    private static String asJsonString(final Object obj) {
-        try {
-            return objectMapper.writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Before
-    public void init() {
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(notifyApi)
-                .setMessageConverters(mappingJackson2HttpMessageConverter)
-                .build();
-    }
-
-    // ************************ \\
-    //         generic          \\
-    // ************************ \\
-
-    private void assert_changeState(What what,
-                                    DemoEvent event,
-                                    DemoState expectedState,
-                                    AbstractStatePersist mock) throws Exception {
-        assert_changeState(what.name().toLowerCase(), event, expectedState, mock);
-    }
-
-    @SuppressWarnings("unchecked")
-    private void assert_changeState(String what,
-                                    DemoEvent event,
-                                    DemoState expectedState,
-                                    AbstractStatePersist mock) throws Exception {
-        // data
+    @Test
+    public void test_changeOrderState() throws Exception {
+        // given
         String code = "CODE";
 
-        // mock
-        doReturn(expectedState).when(mock).change(anyString(), any(DemoEvent.class));
+        MockMvc mockMvc = MockMvcBuilders
+                .standaloneSetup(notifyApi)
+                .build();
 
-        // call
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/notify/" + what);
+        doNothing().when(orderStatePersist).change(anyString(), any(OrderEvent.class));
+
+        // when
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.put("/notify/" + What.ORDER.name().toLowerCase());
 
         requestBuilder.param("code", code)
-                .param("event", event.name());
+                .param("event", OrderEvent.RECEIPT.name());
 
         mockMvc.perform(requestBuilder)
 //               .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(MockMvcResultMatchers.content().json(asJsonString(expectedState)))
-        ;
+                .andExpect(MockMvcResultMatchers.status().isOk());
 
-        // assertions
+        // then
         ArgumentCaptor<String> codeArgumentCaptor = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<DemoEvent> eventArgumentCaptor = ArgumentCaptor.forClass(DemoEvent.class);
-        verify(mock, times(1)).change(codeArgumentCaptor.capture(), eventArgumentCaptor.capture());
+        ArgumentCaptor<OrderEvent> eventArgumentCaptor = ArgumentCaptor.forClass(OrderEvent.class);
+
+        verify(orderStatePersist, times(1)).change(codeArgumentCaptor.capture(), eventArgumentCaptor.capture());
+
         assertThat(codeArgumentCaptor.getValue()).isEqualTo(code);
-        assertThat(eventArgumentCaptor.getValue()).isEqualTo(event);
+        assertThat(eventArgumentCaptor.getValue()).isEqualTo(OrderEvent.RECEIPT);
 
-        verifyNoMoreInteractions(mock);
+        verifyNoMoreInteractions(orderStatePersist);
     }
-
-    // ************************ \\
-    //           ORDER          \\
-    // ************************ \\
-
-    @Test
-    public void test_changeOrderState() throws Exception {
-        // call & assert
-        assert_changeState(What.ORDER, OrderEvent.RECEIPT, OrderState.DONE, orderStatePersist);
-    }
-
 }
--- src/test/java/fr/demo/state/order/config/OrderMachineConfigTest.java
+++ src/test/java/fr/demo/state/order/config/OrderMachineConfigTest.java
@@ -1,51 +1,173 @@
 package fr.demo.state.order.config;
 
+import fr.demo.state.common.MessageService;
 import fr.demo.state.common.What;
 import fr.demo.state.order.OrderEvent;
 import fr.demo.state.order.OrderState;
+import fr.demo.state.order.guard.ChoiceGuard;
+import fr.demo.state.order.guard.PaymentGuard;
+import fr.demo.state.pack.PackEvent;
+import fr.demo.state.pack.data.PackDao;
+import fr.demo.state.pack.data.PackStatePersist;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.test.StateMachineTestPlan;
 import org.springframework.statemachine.test.StateMachineTestPlanBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:order-machine-context-test.xml")
 public class OrderMachineConfigTest {
 
     @Autowired
-    @SuppressWarnings("SpringJavaAutowiringInspection")
     private StateMachineFactory<OrderState, OrderEvent> orderMachineFactory;
 
+    @SpyBean
+    private MessageService messageService;
+
+    @MockBean
+    private PaymentGuard paymentGuard;
+
+    @MockBean
+    private ChoiceGuard choiceGuard;
+
+    @MockBean
+    private PackDao packDao;
+    @MockBean
+    private PackStatePersist packStatePersist;
+
     @Test
-    public void test() throws Exception {
+    public void test_guard_false() throws Exception {
+        // given
+        doReturn(false).when(paymentGuard).evaluate(any());
+
         StateMachineTestPlan<OrderState, OrderEvent> plan =
                 StateMachineTestPlanBuilder.<OrderState, OrderEvent>builder()
                         .stateMachine(orderMachineFactory.getStateMachine(What.ORDER.name()))
-                        .step().
-                        expectState(OrderState.INITIAL)
+                        .step()
+                        .expectState(OrderState.INITIAL)
+
                         .and()
-                        .step().
-                        sendEvent(OrderEvent.CREATE).
-                        expectState(OrderState.DRAFT)
+                        .step()
+                        .sendEvent(OrderEvent.CREATE)
+                        .expectState(OrderState.DRAFT)
+
                         .and()
-                        .step().
-                        sendEvent(OrderEvent.VALIDATE).
-                        expectState(OrderState.PREPARING)
+                        .step()
+                        .sendEvent(OrderEvent.VALIDATE)
+                        .expectState(OrderState.WAITING_PAYMENT)
+
                         .and()
-                        .step().
-                        sendEvent(OrderEvent.SEND).
-                        expectState(OrderState.DELIVERING)
-                        .and()
-                        .step().
-                        sendEvent(OrderEvent.RECEIPT).
-                        expectState(OrderState.DONE)
+                        .step()
+                        .sendEvent(OrderEvent.PAY)
+                        .expectState(OrderState.WAITING_PAYMENT)
+
                         .and()
                         .build();
         plan.test();
+
+        verifyZeroInteractions(packDao);
+        verifyZeroInteractions(packStatePersist);
+        verifyZeroInteractions(messageService);
+    }
+
+    @Test
+    public void test_guard_true_and_choice_false() throws Exception {
+        // given
+        doReturn(true).when(paymentGuard).evaluate(any());
+        doReturn(false).when(choiceGuard).evaluate(any());
+
+        StateMachineTestPlan<OrderState, OrderEvent> plan =
+                StateMachineTestPlanBuilder.<OrderState, OrderEvent>builder()
+                        .stateMachine(orderMachineFactory.getStateMachine(What.ORDER.name()))
+                        .step()
+                        .expectState(OrderState.INITIAL)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.CREATE)
+                        .expectState(OrderState.DRAFT)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.VALIDATE)
+                        .expectState(OrderState.WAITING_PAYMENT)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.PAY)
+                        .expectState(OrderState.TO_PREPARE)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.PREPARE)
+                        .expectState(OrderState.PREPARING)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.SEND)
+                        .expectState(OrderState.DELIVERING)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.RECEIPT)
+                        .expectState(OrderState.DONE)
+
+                        .and()
+                        .build();
+        plan.test();
+
+        verify(packDao).create(null); // fixme the code on the context
+        verify(packStatePersist).change(null,  PackEvent.CREATE);
+        verify(messageService, times(2)).sendMail(anyString());
+    }
+
+    @Test
+    public void test_guard_true_and_choice_true() throws Exception {
+        // given
+        doReturn(true).when(paymentGuard).evaluate(any());
+        doReturn(true).when(choiceGuard).evaluate(any());
+
+        StateMachineTestPlan<OrderState, OrderEvent> plan =
+                StateMachineTestPlanBuilder.<OrderState, OrderEvent>builder()
+                        .stateMachine(orderMachineFactory.getStateMachine(What.ORDER.name()))
+                        .step()
+                        .expectState(OrderState.INITIAL)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.CREATE)
+                        .expectState(OrderState.DRAFT)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.VALIDATE)
+                        .expectState(OrderState.WAITING_PAYMENT)
+
+                        .and()
+                        .step()
+                        .sendEvent(OrderEvent.PAY)
+                        .expectState(OrderState.DONE)
+
+                        .and()
+                        .build();
+        plan.test();
+
+        verifyZeroInteractions(packDao);
+        verifyZeroInteractions(packStatePersist);
+        verifyZeroInteractions(messageService);
     }
 
     @Test
@@ -53,19 +175,26 @@ public class OrderMachineConfigTest {
         StateMachineTestPlan<OrderState, OrderEvent> plan =
                 StateMachineTestPlanBuilder.<OrderState, OrderEvent>builder()
                         .stateMachine(orderMachineFactory.getStateMachine(What.ORDER.name()))
-                        .step().
-                        expectState(OrderState.INITIAL)
+                        .step()
+                        .expectState(OrderState.INITIAL)
+
                         .and()
-                        .step().
-                        sendEvent(OrderEvent.CREATE).
-                        expectState(OrderState.DRAFT)
+                        .step()
+                        .sendEvent(OrderEvent.CREATE)
+                        .expectState(OrderState.DRAFT)
+
                         .and()
-                        .step().
-                        sendEvent(OrderEvent.CANCEL).
-                        expectState(OrderState.CANCELED)
+                        .step()
+                        .sendEvent(OrderEvent.CANCEL)
+                        .expectState(OrderState.CANCELED)
+
                         .and()
                         .build();
         plan.test();
+
+        verifyZeroInteractions(packDao);
+        verifyZeroInteractions(packStatePersist);
+        verifyZeroInteractions(messageService);
     }
 
 }
