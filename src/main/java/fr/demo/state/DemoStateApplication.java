a95028f3c42e9a93dead4f4342ab62190935a089
Le Gall Benoît <benoit.legall@thanksys.com>
2019-04-09 09:17:44 +0200

    code review

--- src/main/java/fr/demo/state/DemoStateApplication.java
+++ src/main/java/fr/demo/state/DemoStateApplication.java
@@ -1,14 +1,12 @@
 package fr.demo.state;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@ConfigurationProperties
 @SpringBootApplication(scanBasePackages = "fr.demo.state")
 public class DemoStateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoStateApplication.class, args);
+        new SpringApplicationBuilder(DemoStateApplication.class).run(args);
     }
 }
